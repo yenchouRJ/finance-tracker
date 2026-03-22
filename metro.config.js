dd881c914106ac25e9815468eb5465c64d9f6dfe
joey <sgreenjoeyboy@gmail.com>
2026-03-22 12:14:22 +0800

    First commit

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { withTamagui } = require('@tamagui/metro-plugin');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './src/theme/tamagui.config.ts',
+  outputCSS: './src/theme/tamagui-web.css',
+});
